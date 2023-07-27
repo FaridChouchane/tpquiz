e49ae8bb2a40c32924dcbc00c9504f2338d15abb
Angelisium <angelisium@daum.net>
2023-07-27 19:00:21 +0200

    Update Countrie.java

---
+++ src/main/java/com/tpquiz/tpquiz/entity/Countrie.java
@@ -0,0 +1,19 @@
+package com.tpquiz.tpquiz.entity;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Table(name = "countries")
+public class Countrie {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	public Integer id;
+
+	public String name;
+
+	public String capital;
+}
