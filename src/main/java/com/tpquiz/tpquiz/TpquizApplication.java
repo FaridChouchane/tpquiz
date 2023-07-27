785b0adf5ec602f462cea8c8398b81220da0887d
Farid CHOUCHANE <chouchanefarid@gmail.com>
2023-07-27 16:24:00 +0200

    first commit

---
+++ src/main/java/com/tpquiz/tpquiz/TpquizApplication.java
@@ -0,0 +1,13 @@
+package com.tpquiz.tpquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpquizApplication.class, args);
+	}
+
+}
