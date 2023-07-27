18dea08f394585d6413dcd5ef3c92b126b8ffc19
Angelisium <angelisium@daum.net>
2023-07-27 18:06:19 +0200

    Create ApplicationTests.java

---
+++ src/test/java/com/tpquiz/tpquiz/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpquiz.tpquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
