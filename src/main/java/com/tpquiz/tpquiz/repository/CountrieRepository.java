2b38825b3063098f4520670ad7614b738e2b71bc
Angelisium <angelisium@daum.net>
2023-07-27 19:00:30 +0200

    Update CountrieRepository.java

---
+++ src/main/java/com/tpquiz/tpquiz/repository/CountrieRepository.java
@@ -0,0 +1,8 @@
+package com.tpquiz.tpquiz.repository;
+
+import com.tpquiz.tpquiz.entity.Countrie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CountrieRepository extends JpaRepository<Countrie, Integer> {
+
+}
