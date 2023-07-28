ab52e26a98fc4c94aafad2a9249f365d7f53e9dd
Fab <fabien@elixeer.fr>
2023-07-28 10:07:35 +0200

    refreash

--- src/main/java/tp/quiz/entity/Countrie.java
+++ src/main/java/tp/quiz/entity/Countrie.java
@@ -1,6 +1,7 @@
 package com.tpquiz.tpquiz.entity;
 
 import jakarta.persistence.Id;
+//import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,8 +10,10 @@ import jakarta.persistence.GenerationType;
 @Entity
 @Table(name = "countries")
 public class Countrie {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	//@SequenceGenerator(name = "countries_seq", sequenceName = "countries_seq")
 	public Integer id;
 
 	public String name;
--- src/test/java/tp/quiz/QuizApplicationTests.java
+++ src/test/java/tp/quiz/QuizApplicationTests.java
@@ -1,10 +1,10 @@
-package com.tpquiz.tpquiz;
+package tp.quiz;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class QuizApplicationTests {
 
 	@Test
 	void contextLoads() {
